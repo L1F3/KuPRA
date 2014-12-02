98a85441e3a73fccceb6230885c13c77833d119c
Tomas <zvaliauskas.tomas@gmail.com>
2014-12-02 20:20:15 +0200

    Generics

---
+++ src/main/java/lt/vu/mif/ps5/kupra/dao/implementation/GenericDaoImpl.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.metasite.des.dao.implementation;
+
+
+import java.util.List;
+import net.metasite.des.dao.GenericDao;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+
+public abstract class GenericDaoImpl<T> implements GenericDao<T> {
+    private SessionFactory sessionFactory;
+
+    private Class<T> persistingClass;
+
+    protected GenericDaoImpl(SessionFactory sessionFactory, Class<T> persistingClass) {
+        this.sessionFactory = sessionFactory;
+        this.persistingClass = persistingClass;
+    }
+
+    @Override
+    public List<T> getAll() {
+        Session sess = getSession();
+
+        @SuppressWarnings("unchecked")
+        List<T> list = (List<T>)sess.createCriteria(persistingClass).list();
+        return list;
+    }
+
+
+    @Override
+    public T get(long id) {
+        return (T) getSession().get(persistingClass, id);
+
+    }
+    
+
+    @Override
+    public void persist(T entity) {
+        getSession().persist(entity);
+    }
+
+    protected Session getSession() throws HibernateException {
+        Session sess = sessionFactory.getCurrentSession();
+        return sess;
+    }
+}
