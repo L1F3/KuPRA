07dc861b82a5d5bbd5eaed6892a2612b686aa585
acer <acer@172.26.199.50>
2014-12-02 21:34:28 +0200

    siaip

--- src/main/java/lt/vu/mif/ps5/kupra/dao/implementation/ProductDaoImpl.java
+++ src/main/java/lt/vu/mif/ps5/kupra/dao/implementation/ProductDaoImpl.java
@@ -1,7 +1,23 @@
-package lt.vu.mif.ps5.kupra.dao;
+package lt.vu.mif.ps5.kupra.dao.implementation;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import lt.vu.mif.ps5.kupra.entity.Product;
+import lt.vu.mif.ps5.kupra.dao.ProductDao;
 
-public interface ProductDao extends GenericDao<Product> {
+@Component
+public class ProductDaoImpl extends GenericDaoImpl<Product> implements ProductDao {
+	
+	@Autowired
+    public ProductDaoImpl(SessionFactory sessionFactory) {
+        super(sessionFactory, Product.class);
+    }
+
+	@Override
+    public void delete(long productId) {
+        Product product = get(productId);
+        getSession().delete(product);
+    }
 	
 }
