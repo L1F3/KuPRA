634bb2053e2bb950a301593b46d3ee666d3ccc31
Tomas <zvaliauskas.tomas@gmail.com>
2014-12-02 21:19:21 +0200

    Unit dao impl

--- src/main/java/lt/vu/mif/ps5/kupra/dao/UserDao.java
+++ src/main/java/lt/vu/mif/ps5/kupra/dao/UserDao.java
@@ -3,5 +3,6 @@ package lt.vu.mif.ps5.kupra.dao;
 import lt.vu.mif.ps5.kupra.entity.User;
 
 public interface UserDao extends GenericDao<User>{
-	
+
+	public void delete(long unitId);
 }
--- src/main/java/lt/vu/mif/ps5/kupra/dao/implementation/UnitDaoImpl.java
+++ src/main/java/lt/vu/mif/ps5/kupra/dao/implementation/UnitDaoImpl.java
@@ -2,7 +2,10 @@ package lt.vu.mif.ps5.kupra.dao.implementation;
 
 import java.util.List;
 import java.util.Set;
+
+import lt.vu.mif.ps5.kupra.dao.UnitDao;
 import lt.vu.mif.ps5.kupra.entity.Unit;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Order;
@@ -11,10 +14,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UnitDaoImpl extends GenericDaoImpl<Expo> implements UnitDao {
+public class UnitDaoImpl extends GenericDaoImpl<Unit> implements UnitDao {
 
     @Autowired
     public UnitDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory, Unit.class);
     }
+
+    @Override
+    public void delete(long unitId) {
+        Unit unit = get(unitId);
+        getSession().delete(unit);
+    }
+    
+    
 }
