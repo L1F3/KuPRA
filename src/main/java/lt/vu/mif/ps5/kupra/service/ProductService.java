df98009e7d202828007d71ad1c69b3143675a73f
vytaus <vytautaslatvys@gmail.com>
2014-12-03 15:46:51 +0200

    ProductService interface base methods

---
+++ src/main/java/lt/vu/mif/ps5/kupra/service/ProductService.java
@@ -0,0 +1,19 @@
+package lt.vu.mif.ps5.kupra.service;
+
+import java.sql.Blob;
+import java.util.List;
+import java.util.Set;
+
+import lt.vu.mif.ps5.kupra.entity.Product;
+import lt.vu.mif.ps5.kupra.entity.Unit;
+
+public interface ProductService {
+
+	Product getProduct(long productId);
+
+	List<Product> getAll();
+
+	long addProduct(String productName, Set<Unit> units, String description,
+			String imageName, String imageType, Blob image);
+
+}
