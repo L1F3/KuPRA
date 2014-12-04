09f7eb69448d4ee01c9352f9336fd976f8960374
Tomas <zvaliauskas.tomas@gmail.com>
2014-12-04 18:42:58 +0200

    registercontroller

--- src/main/java/lt/vu/mif/ps5/kupra/controller/RegisterController.java
+++ src/main/java/lt/vu/mif/ps5/kupra/controller/RegisterController.java
@@ -4,6 +4,7 @@ import java.util.logging.Logger;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,12 +13,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import lt.vu.mif.ps5.kupra.form.UserForm;
+import lt.vu.mif.ps5.kupra.service.UserService;
 
 @Controller
 public class RegisterController {
 
 	static Logger log = Logger.getLogger(RegisterController.class.getName());
 
+    private final UserService userService;
+
+    @Autowired
+    public RegisterController(UserService userService) {
+        this.userService = userService;
+    }
+	
 	@RequestMapping(value = "/register", method = RequestMethod.GET)
 	public ModelAndView registerPage() {
 		return new ModelAndView("register").addObject(new UserForm());
@@ -27,9 +36,23 @@ public class RegisterController {
 	public ModelAndView registerDo(@Valid @ModelAttribute UserForm userForm,
 			Errors errors) {
 		if (errors.hasErrors()) {
+			//Pvz kaip reik rejectint patikrinus.
+			//errors.rejectValue("name", "msg", "LOPASTU");
 			return new ModelAndView("register").addObject(userForm);
 		}
-		return new ModelAndView("register").addObject(new UserForm());
+		errors.rejectValue("name", "msg", "erroras, truksta kazko");
+		return new ModelAndView("register").addObject(userForm);
+		/*userService.addUser(
+				userForm.getLoginname(), 
+				userForm.getUsername(),
+				userForm.getPassword(),
+				userForm.getEmail(),
+				userForm.getName(), 
+				userForm.getLastname(),
+				userForm.getAddress(),
+				userForm.getRole());
+		return new ModelAndView("redirect:/home");*/
+		
 	}
 
 }
