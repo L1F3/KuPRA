ab96a2ad89059c211bdbce87de7b7d137168b69d
vytaus <vytautaslatvys@gmail.com>
2014-12-04 20:24:02 +0200

    MealsController updated

--- src/main/java/lt/vu/mif/ps5/kupra/controller/MealsController.java
+++ src/main/java/lt/vu/mif/ps5/kupra/controller/MealsController.java
@@ -1,11 +1,7 @@
 package lt.vu.mif.ps5.kupra.controller;
 
-import java.util.List;
-import java.util.Set;
 import java.util.logging.Logger;
 
-import lt.vu.mif.ps5.kupra.entity.Recipe;
-import lt.vu.mif.ps5.kupra.entity.User;
 import lt.vu.mif.ps5.kupra.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,20 +23,7 @@ public class MealsController {
     }
 	
 	@RequestMapping(value = "/meals", method = RequestMethod.GET)
-	public ModelAndView eatsList() {
-		
-		Set<Recipe> meals = null;
-		
-		List<User> users = userService.getAll();
-		
-		String username = userService.getUsername();
-		
-		for (User user : users) {
-			if (user.getName().equals(username)) {
-				meals = user.getMeals();
-			}
-		}
-		
-		return new ModelAndView("mealslist").addObject("meals", meals);
+	public ModelAndView eatsList() {		
+		return new ModelAndView("mealslist").addObject("meals", userService.getMeals());
 	}
 }
