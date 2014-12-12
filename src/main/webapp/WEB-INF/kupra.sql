1a833400cc9aa83d3f9dc44bc12d7c770c1ff32b
acer <acer@172.26.198.137>
2014-12-12 22:40:02 +0200

    patobulinta duombaze

--- src/main/webapp/WEB-INF/kupra.sql
+++ src/main/webapp/WEB-INF/kupra.sql
@@ -13,26 +13,6 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Database: `kupra`
 -- 
 
--- --------------------------------------------------------
-
--- 
--- Table structure for table `ingredients`
--- 
-
-CREATE TABLE `ingredients` (
-  `ingrId` int(10) NOT NULL auto_increment,
-  `recId` int(10) NOT NULL,
-  `productId` int(10) NOT NULL,
-  `unitId` int(10) NOT NULL,
-  `amount` int(10) NOT NULL,
-  PRIMARY KEY  (`ingrId`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
-
--- 
--- Dumping data for table `ingredients`
--- 
-
-INSERT INTO `ingredients` VALUES (1, 1, 1, 1, 200);
 
 -- --------------------------------------------------------
 
@@ -54,14 +34,23 @@ CREATE TABLE `products` (
 -- Dumping data for table `products`
 -- 
 
-INSERT INTO `products` VALUES (1, 'Kiausiniai', 'sdadassadasda', NULL, NULL, NULL);
+INSERT INTO `products` VALUES (1, 'Kiausiniai', 'Kiausiniai rudu lukstu.', NULL, NULL, NULL);
+INSERT INTO `products` VALUES (2, 'Spageciai', 'Spageciai.', NULL, NULL, NULL);
+INSERT INTO `products` VALUES (3, 'Suris', 'Fermentinis suris.', NULL, NULL, NULL);
+INSERT INTO `products` VALUES (4, 'Miltai', 'Kvietiniai miltai.', NULL, NULL, NULL);
+INSERT INTO `products` VALUES (5, 'Farsas', 'Jautienos farsas.', NULL, NULL, NULL);
+INSERT INTO `products` VALUES (6, 'Pienas', 'Pusriebis pienas.', NULL, NULL, NULL);
+INSERT INTO `products` VALUES (7, 'Grietinele', 'Grietinele.', NULL, NULL, NULL);
+INSERT INTO `products` VALUES (8, 'Padazas', 'Pomidoru padazas.', NULL, NULL, NULL);
+INSERT INTO `products` VALUES (9, 'Alyvuoges', 'Juodosios alyvuoges.', NULL, NULL, NULL);
+INSERT INTO `products` VALUES (10, 'Bazilikai', 'Sviezi bazilikai.', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
 -- 
 -- Table structure for table `recipe_product`
 -- 
-
+/*
 CREATE TABLE `recipe_product` (
   `recId` int(10) NOT NULL,
   `productId` int(10) NOT NULL
@@ -71,8 +60,12 @@ CREATE TABLE `recipe_product` (
 -- Dumping data for table `recipe_product`
 -- 
 
-INSERT INTO `recipe_product` VALUES (1, 1); 
-
+INSERT INTO `recipe_product` VALUES (1, 1);
+INSERT INTO `recipe_product` VALUES (2, 2);
+INSERT INTO `recipe_product` VALUES (2, 3);
+INSERT INTO `recipe_product` VALUES (3, 4);
+INSERT INTO `recipe_product` VALUES (3, 5);
+*/
 -- --------------------------------------------------------
 
 -- 
@@ -120,7 +113,11 @@ CREATE TABLE `units` (
 -- Dumping data for table `units`
 -- 
 
-INSERT INTO `units` VALUES (1, 'kilogramai', 'kg');
+INSERT INTO `units` VALUES (1, 'Kilogramai', 'kg');
+INSERT INTO `units` VALUES (2, 'Gramai', 'g');
+INSERT INTO `units` VALUES (3, 'Litrai', 'l');
+INSERT INTO `units` VALUES (4, 'Mililitrai', 'ml');
+INSERT INTO `units` VALUES (5, 'Vienetai', 'vnt');
 
 -- --------------------------------------------------------
 
@@ -137,6 +134,61 @@ CREATE TABLE `unit_product` (
 -- Dumping data for table `unit_product`
 -- 
 
+INSERT INTO `unit_product` VALUES (1, 5);
+INSERT INTO `unit_product` VALUES (2, 1);
+INSERT INTO `unit_product` VALUES (2, 2);
+INSERT INTO `unit_product` VALUES (3, 1);
+INSERT INTO `unit_product` VALUES (3, 2);
+INSERT INTO `unit_product` VALUES (4, 1);
+INSERT INTO `unit_product` VALUES (4, 2);
+INSERT INTO `unit_product` VALUES (4, 3);
+INSERT INTO `unit_product` VALUES (4, 4);
+INSERT INTO `unit_product` VALUES (5, 1);
+INSERT INTO `unit_product` VALUES (5, 2);
+INSERT INTO `unit_product` VALUES (6, 3);
+INSERT INTO `unit_product` VALUES (6, 4);
+INSERT INTO `unit_product` VALUES (7, 3);
+INSERT INTO `unit_product` VALUES (7, 4);
+INSERT INTO `unit_product` VALUES (8, 3);
+INSERT INTO `unit_product` VALUES (8, 4);
+INSERT INTO `unit_product` VALUES (9, 1);
+INSERT INTO `unit_product` VALUES (9, 2);
+INSERT INTO `unit_product` VALUES (9, 5);
+INSERT INTO `unit_product` VALUES (10, 1);
+INSERT INTO `unit_product` VALUES (10, 2);
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `ingredients`
+-- 
+
+CREATE TABLE `ingredients` (
+  `ingrId` int(10) NOT NULL auto_increment,
+  `recId` int(10) NOT NULL,
+  `productId` int(10) NOT NULL,
+  `unitId` int(10) NOT NULL,
+  `amount` int(10) NOT NULL,
+  PRIMARY KEY  (`ingrId`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+-- 
+-- Dumping data for table `ingredients`
+-- 
+
+INSERT INTO `ingredients` VALUES (1, 1, 1, 5, 3);
+INSERT INTO `ingredients` VALUES (2, 2, 2, 2, 300);
+INSERT INTO `ingredients` VALUES (3, 2, 3, 2, 100);
+INSERT INTO `ingredients` VALUES (4, 3, 4, 2, 100);
+INSERT INTO `ingredients` VALUES (5, 3, 5, 2, 100);
+INSERT INTO `ingredients` VALUES (6, 4, 3, 2, 100);
+INSERT INTO `ingredients` VALUES (7, 4, 4, 2, 100);
+INSERT INTO `ingredients` VALUES (8, 4, 5, 2, 100);
+INSERT INTO `ingredients` VALUES (9, 4, 8, 4, 200);
+INSERT INTO `ingredients` VALUES (10, 4, 9, 2, 100);
+INSERT INTO `ingredients` VALUES (11, 4, 10, 2, 100);
+INSERT INTO `ingredients` VALUES (12, 5, 6, 4, 500);
+INSERT INTO `ingredients` VALUES (13, 5, 7, 4, 100);
 
 -- --------------------------------------------------------
 
